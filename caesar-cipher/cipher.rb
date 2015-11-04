3d653200ae9a3898eab85f1e3768b52764a0b3a4
Hubert Pompecki <hpompecki@gmail.com>
2015-11-04 21:39:30 +0000

    working version of caesar cipher

---
+++ caesar-cipher/cipher.rb
@@ -0,0 +1,19 @@
+def caesar_cipher(text, offset)
+  new_text = ""
+  text.each_char do |character|
+    if ('A'..'Z').include?(character) 
+      new_char = character.ord + offset
+      new_char -= 26 if new_char > 90
+      new_text << new_char.chr
+    elsif ('a'..'z').include?(character)
+      new_char = character.ord + offset
+      new_char -= 26 if new_char > 122
+      new_text << new_char.chr
+    else
+      new_text << character
+    end
+  end
+  new_text
+end
+
+puts caesar_cipher("What a string!", 5)
